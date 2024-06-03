bbd6bd7aebf093c3f3a141cc7cbfac9a03b8d0cf
dat nguyen <nguyentuandat2809@gmail.com>
2024-06-03 21:58:18 +0700

    add BusFilterScreen

---
+++ src/components/busfilter/BusFilterScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./BusFilterScreen.css";
+
+const BusFilterScreen = () => {
+  return (
+    <div>
+      <div className="titlePage">
+        <h1>Lọc chuyến xe</h1>
+      </div>
+    </div>
+  );
+};
+
+export default BusFilterScreen;
